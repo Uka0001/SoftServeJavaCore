995e5110479aea4b9e05c56f4f60e28bd2e60774
Uka0001 <ukraine0001@gmail.com>
2022-08-05 20:57:12 +0300

    practical task 9-1 completed

---
+++ hw9_Collections/pt1/CollectionPt1.java
@@ -0,0 +1,49 @@
+package com.softserve.hometasks.hometask1.hw9_Collections.pt1;
+
+import java.util.*;
+
+public class CollectionPt1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> myCollection = new ArrayList<>();
+        do {
+            myCollection.add(scanner.nextInt());
+        } while (scanner.hasNextInt());
+        System.out.println("Your collection is: " + myCollection);
+        scanner.close();
+        List<Integer> myCollectionBiggerFive = new ArrayList<>();
+        for (Integer each: myCollection) {
+            if (each > 5){
+                myCollectionBiggerFive.add(each);
+            }
+        }
+        System.out.println("Your new collection without elements>5: " + myCollectionBiggerFive);
+
+        for (Integer each20: myCollection){
+            if (each20 > 20) {
+                myCollectionBiggerFive.remove(each20);
+            }
+        }
+        System.out.println("Your new collection without elements>20: " + myCollectionBiggerFive);
+
+        myCollectionBiggerFive.add(2, 1);
+        myCollectionBiggerFive.add(8, -3);
+        myCollectionBiggerFive.add(5, -4);
+
+        Collections.sort(myCollectionBiggerFive); ////sorting in descending(natural) order
+        //Collections.sort(myCollectionBiggerFive, Collections.reverseOrder()); //sorting in reverse order
+
+            for (Integer each: myCollectionBiggerFive) {
+            System.out.println("position – " + myCollectionBiggerFive.indexOf(each) + ", value of element – " + each);
+        }
+    }
+}
+// values for scanner:
+// 5 34 23 12 1 2 67 78 45 34 23 1 2 3 4 6 7 8 9 10 11 13 null
+//
+/*Declare collection myCollection of 10 integers and fill it (from the console or random).
+Find and save in list newCollection all positions of element more than 5 in the collection. Print newCollection
+Remove from collection myCollection elements, which are greater then 20. Print result
+Insert elements 1, -3, -4 in positions 2, 8, 5. Print result in the format: “position – xxx, value of element – xxx”
+Sort and print collection
+*/
---
+++ hw9_Collections/pt2/Employee.java
@@ -0,0 +1,9 @@
+package com.softserve.hometasks.hometask1.hw9_Collections.pt2;
+
+public class Employee {
+}
+/*In the main() method declare map employeeMap of pairs <Integer, String>.
+Add to employeeMap seven pairs (ID, name) of some persons. Display the map on the screen.
+Ask user to enter ID, then find and write corresponding name from your map. If you can't find this ID - say about it to user (use function containsKey()).
+Ask user to enter name, verify than you have name in your map and write corresponding ID. If you can't find this name - say about it to user (use function containsValue()).
+*/
